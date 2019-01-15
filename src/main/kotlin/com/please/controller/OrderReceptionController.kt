a9abd5e5983778b5b5b494115c81a62fc7b23c39
jeonchanghyeon <jchyeon0814@naver.com>
2019-01-15 10:09:25 +0900

    주문 접수 컨트롤러 생성

---
+++ src/main/kotlin/com/please/controller/OrderReceptionController.kt
@@ -0,0 +1,33 @@
+package com.please.controller
+
+import com.please.persistence.getAuthInfo
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/order-reception")
+class OrderReceptionController {
+    @GetMapping
+    fun showAcceptOrder(): String {
+        val authInfo = getAuthInfo()!!
+        when (authInfo.group) {
+            7 -> {
+                return "order_reception/reception_head"
+            }
+            6 -> {
+                return "order_reception/reception_distrib"
+            }
+            in 4..5 -> {
+                return "order_reception/reception_branch"
+            }
+            3 -> {
+                return "order_reception/reception_shop"
+            }
+            2 -> {
+                return "order_reception/reception_rider"
+            }
+        }
+        return "login"
+    }
+}
