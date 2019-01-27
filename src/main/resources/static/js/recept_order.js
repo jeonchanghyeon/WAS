22197bcb0ec4e50300048c069eaee96027cd4768
pip2150 <dhrcogns2002@hanmail.net>
2019-01-28 06:00:11 +0900

    접수 헤더 추가

--- src/main/resources/static/js/recept_order.js
+++ src/main/resources/static/js/recept_order.js
@@ -1,7 +1,9 @@
 import {withPostMethod} from "./ajax.js";
 import {getMeta} from './meta.js'
+import {$} from "./element.js"
+import {modalOpen} from "./popup_search_address.js"
 
-let receptForm = document.getElementById("recept_form");
+const receptForm = $("recept_form");
 
 receptForm.onsubmit = function () {
     const formData = new FormData(this);
@@ -10,55 +12,56 @@ receptForm.onsubmit = function () {
         "/order-reception/efg",
         formData,
         () => {
-
         },
-        csrfHeader,
-        csrfToken
+        getMeta("_csrf_header"),
+        getMeta("_csrf")
     );
 
     return false;
 };
 
-const sum = document.getElementById("sum");
+const sum = $("sum");
+const additionalMenuPrice = $("additional-menuPrice");
+const menuPrice = $("menu-price");
 
-const additionalMenuPrice = document.getElementById("additional-menuPrice");
-const menuPrice = document.getElementById("menu-price");
+menuPrice.onchange
+    = additionalMenuPrice.onchange
+    = () => {
+    sum.value
+        = parseInt(menuPrice.value)
+        + parseInt(additionalMenuPrice.value)
+        + "원";
+};
 
-menuPrice.onchange =
-    additionalMenuPrice.onchange = () => {
-        sum.value = parseInt(menuPrice.value) + parseInt(additionalMenuPrice.value) + "원";
-    };
-
-const deliveryCost = document.getElementById("delivery-cost");
-const additionalCost = document.getElementById("additional-cost");
-const addCost = document.getElementById("add-cost");
-const extraCharge = document.getElementById("extra-charge");
-const btnAddress = document.getElementById("btn_address");
+const deliveryCost = $("delivery-cost");
+const additionalCost = $("additional-cost");
+const addCost = $("add-cost");
+const extraCharge = $("extra-charge");
+const btnAddress = $("btn_address");
+const btnBranchName = $("btn_branch_name");
+const btnShopName = $("btn_shop_name");
 
 btnAddress.onclick = () => {
-    const windowModal = document.getElementById("modal");
-    windowModal.style.visibility = "visible";
+    const shopId = $("shop_id").value;
+    modalOpen(shopId);
+
     return false;
 };
 
-btnAddress.onsubmit = () => false;
+btnBranchName.onsubmit
+    = btnShopName.onsubmit
+    = btnAddress.onsubmit
+    = () => false;
 
-
-addCost.onchange =
-    extraCharge.onchange = () => {
-        additionalCost.value = parseInt(deliveryCost.value)  + parseInt(addCost.value) + parseInt(extraCharge.value) + "원";
-    };
-
-
-const csrfToken = getMeta("_csrf");
-const csrfHeader = getMeta("_csrf_header");
-
-const loadPint = () => document.getElementById("point").value = 0;
-const loadShopId = () => document.getElementById("shop_id").value = 1;
-
-loadPint();
-loadShopId();
-
+addCost.onchange
+    = extraCharge.onchange
+    = () => {
+    additionalCost.value
+        = parseInt(deliveryCost.value)
+        + parseInt(addCost.value)
+        + parseInt(extraCharge.value)
+        + "원";
+};
 
 // TODO 상품 메뉴
 // TODO 대행료 지불방법 불러오기
