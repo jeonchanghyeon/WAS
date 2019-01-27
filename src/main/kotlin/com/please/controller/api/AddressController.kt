7fd46d60f3a6c7c9c07f9e68013990fd0c84cef2
jeonchanghyeon <jchyeon0814@naver.com>
2019-01-27 21:44:40 +0900

    배달가능동 api 수정

--- src/main/kotlin/com/please/controller/api/AddressController.kt
+++ src/main/kotlin/com/please/controller/api/AddressController.kt
@@ -2,6 +2,7 @@ package com.please.controller.api
 
 import com.please.persistence.getAuthInfo
 import com.please.service.AddressService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/address")
 class AddressController {
+    @Autowired
     private lateinit var addressService: AddressService
 
     @GetMapping
@@ -24,11 +26,23 @@ class AddressController {
         }
     }
 
+
+    /*  result ->
+        {   "resultCode", "description",
+            "enableDong": { "id", "districtCode", "siDo", "siGunGu",
+                            "eubMyeonDong", "latitude", "longitude"
+                           }
+        }
+     */
     @GetMapping(value = ["enable-dong"])
-    fun getEnableArea(@RequestParam data: MutableMap<String, Any>): Any {
+    fun getEnableArea(@RequestParam(value = "id") id: Long,
+                      @RequestParam(value = "consonant") consonant: Int): Any {
+        /*  id -> 상점 아이디
+            consonant -> 0:전체, 1:ㄱ, 2:ㄴ ...... 14:ㅎ
+        */
         return try {
             val authInfo = getAuthInfo()!!
-            val value = addressService.getEnableArea(authInfo.authKey, data)
+            val value = addressService.getEnableArea(authInfo.authKey, id, consonant)
             println(value)
             value!!.toString()
         } catch (e: Exception) {
