501a20af83c8c455ea3d2886ee97478f7d40b7c4
pip2150 <dhrcogns2002@hanmail.net>
2019-01-28 05:39:58 +0900

    접수 주소 검색 창 변경

--- src/main/resources/static/js/popup_search_address.js
+++ src/main/resources/static/js/popup_search_address.js
@@ -1,12 +1,11 @@
 import {ajax, withGetMethod} from './ajax.js'
-
-document.body.onload = loadDong;
+import {$} from "./element.js"
 
 //동 이름 불러오기
-function loadDong() {
-    const url = "/abce/ewe";
+const loadEnableDong = () => {
+    const url = "/api/address/enable-dong";
 
-    const formEnableArea = document.getElementById("form_enable_area");
+    const formEnableArea = $("form_enable_area");
     const formData = new FormData(formEnableArea);
 
     withGetMethod(
@@ -14,81 +13,114 @@ function loadDong() {
         formData,
         (obj) => {
             try {
-                const container = document.getElementById("enableArea");
+                const table = $("enable_dong");
                 const enableDong = obj["enableDong"];
 
-                container.innerHTML = "";
+                table.innerHTML = "";
 
                 for (let i = 0; i < enableDong.length; i++) {
 
-                    const div = document.createElement("div");
-                    const span = document.createElement("span");
+                    const dong = enableDong[i];
 
-                    span.innerHTML = enableDong[i]["eubMyeonDong"];
+                    const tr = document.createElement("tr");
+                    const td = document.createElement("td");
+                    const button = document.createElement("button");
 
-                    div.appendChild(span);
-                    container.appendChild(div);
+                    button.innerHTML = dong["eubMyeonDong"];
+
+                    button.onclick = () => {
+                        const jibun = dong["siDo"] + " " + dong["siGunGu"] + " " + dong["eubMyeonDong"]
+                        alert(jibun);
+                        $("jibun").value = jibun;
+                    };
+
+                    td.appendChild(button);
+                    tr.appendChild(td);
+                    table.appendChild(tr);
                 }
-            } catch (error) {
-                console.log(error.message);
+
+            } catch (e) {
+                console.log(e);
             }
+
         });
-}
+};
 
-
-const formAddress = document.getElementById("form_address");
-
-formAddress.onsubmit = searchAddress;
+// $("form_address").onsubmit = searchAddress;
 
 //건물명
-function searchAddress() {
-    let siGunGu;
-    let eubMyeonDong;
-    let siDo;
-
-    const userId = "god";
-    const address = siDo + " " + siGunGu + " " + eubMyeonDong;
-    const pageIndex = 1;
-    const searchType = 1;
-
-    const url = "http://13.209.35.180:1547/address/get?" +
-        "userId=${userId}&" +
-        "address=${address}&" +
-        "pageIndex=${index}&" +
-        "searchType=${searchType}";
+const searchAddress = (userId, address) => {
+    const url = `http://13.209.35.180:1547/address/get?userId=${userId}&address=${address}&searchType=1`;
 
     ajax(
         url,
         "GET",
         (obj) => {
             try {
-                const data = obj["data"];
+                // const data = obj["data"];
+                //
+                // const address = $("address");
+                // for (let i = 0; i < data.length; i++) {
+                //     const tr = document.createElement("tr");
+                //     for (let j = 0; j < 3; j++) {
+                //         const td = document.createElement("td");
+                //
+                //         tr.appendChild(td);
+                //     }
+                //     tr.appendChild(address);
+                // }
 
-                const address = document.getElementById("address");
+                console.log(obj);
 
-                for (let i = 0; i < data.length; i++) {
-                    const tr = document.createElement("tr");
-                    for (let j = 0; j < 3; j++) {
-                        const td = document.createElement("td");
-
-                        tr.appendChild(td);
-                    }
-                    tr.appendChild(address);
-                }
-
-            } catch (error) {
-                console.log(error.message);
+            } catch (e) {
+                console.log(e);
             }
         }
     );
+};
 
-    return false;
+$("btn_close").onclick = () => {
+    modalClose();
+};
+
+//TODO 외곽 클릭시 창 닫기
+
+export const modalClose = () => {
+    $("modal").style.display = "none";
+};
+
+export const modalOpen = (shopId) => {
+    $("modal-shop-id").value = shopId;
+    $("modal").style.display = "inherit";
+};
+
+const consonantArea = $("consonant-area");
+
+const buttons = consonantArea.getElementsByTagName("button");
+
+for (let i = 0; i < buttons.length; i++) {
+
+    buttons[i].onclick = function () {
+        $("consonant").value = this.value;
+        loadEnableDong();
+    };
 }
 
+$("form-jibun").onsubmit = function () {
+    $("jibun").value += " " + $("jibun-detail").value;
 
-const btnClose = document.getElementById("btn_close");
+    const formData = new FormData(this);
 
-btnClose.onclick = () => {
-    const windowModal = document.getElementById("modal");
-    windowModal.style.visibility = "hidden";
+    const userId = 1;
+    const address = formData.get("jibun");
+
+    const url = `http://13.209.35.180:1547/address/get?userId=${userId}&address=${address}&searchType=1`;
+
+    ajax(
+        url,
+        "GET",
+        (obj) => console.log(obj)
+    );
+
+    return false;
 };
