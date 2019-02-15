7dedb4c7f6423bc2e929adc08cb9855560b30200
pip2150 <dhrcogns2002@hanmail.net>
2019-02-15 23:58:34 +0900

    마일리지 모달 헤더에 추가

--- src/main/resources/static/js/point.js
+++ src/main/resources/static/js/point.js
@@ -1,4 +1,5 @@
 import {$} from "./element.js";
+import {numberWithCommas, YYYYmmdd} from "./format.js";
 import {getJSON} from "./ajax.js";
 
 export const loadPoint = () =>
@@ -6,5 +7,41 @@ export const loadPoint = () =>
         (obj) => {
             const point = obj["point"]["point"].toString();
             $("point").innerText = point;
+            $("mileage").innerText = numberWithCommas(point) + "원";
         }
     );
+
+const displayWithdrawalSection = () => {
+    $("withdraw-section").style.display = "flex";
+    $("withdraw-submit-section").style.display = "flex";
+
+    $("transfer-section").style.display = "none";
+    $("deposit-submit-section").style.display = "none";
+};
+
+const displayDepositSection = () => {
+    $("withdraw-section").style.display = "none";
+    $("withdraw-submit-section").style.display = "none";
+
+    $("transfer-section").style.display = "flex";
+    $("deposit-submit-section").style.display = "flex";
+};
+
+$("btn-withdraw").onclick = displayWithdrawalSection;
+$("btn-deposit").onclick = displayDepositSection;
+
+const mileageModal = $('mileage_modal');
+
+$("close-button").onclick = () => {
+    mileageModal.style.display = 'none';
+};
+
+$("point-area").onclick = () => {
+    const date = YYYYmmdd(new Date());
+
+    loadPoint();
+    $("mileage-description").innerHTML = `${date} 현재 마일리지`;
+
+    mileageModal.style.display = 'initial';
+    displayWithdrawalSection();
+};
