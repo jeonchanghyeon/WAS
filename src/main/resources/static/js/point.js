4b4f5111e1f06bba12a86de39cd9c098e4093ed2
pip2150 <dhrcogns2002@hanmail.net>
2019-03-05 03:24:02 +0900

    포인트 인출 계좌 추가 및 자동 계산

--- src/main/resources/static/js/point.js
+++ src/main/resources/static/js/point.js
@@ -3,11 +3,13 @@ import {numberWithCommas, YYYYmmdd} from "./format.js";
 import {getJSON} from "./ajax.js";
 import {addCloseModalEvent} from "./modal.js";
 
+let point_ = null;
 export const loadPoint = () =>
     getJSON("/api/point").then(
         (obj) => {
+            point_ = parseInt(obj["point"]["point"]);
             const point = obj["point"]["point"].toString();
-            $("point").innerText = point;
+            $("point").innerText = numberWithCommas(point);
             $("mileage").innerText = numberWithCommas(point) + "원";
         }
     );
@@ -38,9 +40,34 @@ $("point-area").onclick = () => {
 
     loadPoint();
     $("mileage-description").innerHTML = `${date} 현재 마일리지`;
+    const id = $("userId").value;
+
+    getJSON('api/users/' + id + '/account').then(
+        obj => {
+            const owner = obj["owner"];
+            const account = obj["account"];
+            const bank = obj["bank"];
+
+            $("bank-account").innerHTML = `${owner} ${account} ${bank}`;
+        }
+    ).catch(() => {
+        $("bank-account").innerHTML = '-';
+    });
 
     mileageModal.style.display = 'initial';
     displayWithdrawalSection();
 };
 
 addCloseModalEvent("mileage_modal", "close-button");
+
+$("btn-full-withdraw").onclick = () => {
+    $("amount").value = parseInt($("point").innerText);
+};
+
+$("amount").onchange = () => {
+    const fee = parseInt($("withdraw-fees").value);
+    const amount = parseInt($("amount").value);
+    const point = point_;
+
+    $("deducted-amount").value = numberWithCommas(point - amount - fee) + "원";
+};
